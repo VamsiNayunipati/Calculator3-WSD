dae0df2b84ad8207ace1f5a21cfd48858ba3d4e4
VAMSI KRISHNA NAYUNIPATI <86821610+VamsiNayunipati@users.noreply.github.com>
2024-02-14 16:51:34 -0500

    Update test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -5,7 +5,7 @@ def test_addition():
     '''Test that addition function works '''    
     assert Calculator.add(2,2) == 4
 
-def test_subtraction():
+def test_subtraction(): 
     '''Test that addition function works '''    
     assert Calculator.subtract(2,2) == 0
 
