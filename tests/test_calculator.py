a6c09017ac9da2d704133d5bb8e89e5919e4ef56
VAMSI KRISHNA NAYUNIPATI <86821610+VamsiNayunipati@users.noreply.github.com>
2024-02-20 17:34:02 -0500

    Update test_calculator

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,18 +1,19 @@
 '''My Calculator Test'''
-from calculator import Calculator
+from calculator import add, subtract, multiply, divide
 
 def test_addition():
     '''Test that addition function works '''    
-    assert Calculator.add(2,2) == 4
+    assert add(2,2) == 4
 
-def test_subtraction(): 
-    '''Test that addition function works '''    
-    assert Calculator.subtract(2,2) == 0
+def test_subtraction():
+    '''Test that subtraction function works '''    
+    assert subtract(2,2) == 0
 
-def test_multiply():
-    '''Test that addition function works '''    
-    assert Calculator.multiply(2,2) == 4
+def test_multiplication():
+    '''Test that multiplication function works '''    
+    assert multiply(2,3) == 6
 
-def test_divide():
-    '''Test that addition function works '''    
-    assert Calculator.divide(2,2) == 1
+def test_divition():
+    '''Test that divition function works '''    
+    assert divide(2,2) == 1
+    
